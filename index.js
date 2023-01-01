e912864d601e5f668871c2d43c5a31eda7b5ffb8
Hekmat Mohammed <heka332020@gmail.com>
2023-01-01 20:21:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" },
+    //...
+];
+
+const superbowlWin = (collection) => {
+    for (const ele of collection) {
+        if (ele.result === "W") {
+            return ele.year;
+        }
+    }
+}
